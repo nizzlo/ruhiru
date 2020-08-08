37797f4000fc877acdd49c44a319c6fca5ae0659
nizzlo <nisalsamaranayake@gmail.com>
2020-08-08 15:59:03 +0530

    session saving

--- userinterface/ruhiru/src/app/login/login.component.ts
+++ userinterface/ruhiru/src/app/login/login.component.ts
@@ -34,8 +34,26 @@ export class LoginComponent implements OnInit {
   var url="https://d9edc04fdbc6.ngrok.io/api/v1/user/login"
 
     this.http.post<any>(url,loginData).subscribe(data =>{
+      //console.log(data);
+      console.log(data.user.email);
+      localStorage.setItem('email', data.user.email);
+      console.log(data.user.contact);
+      localStorage.setItem('contact', data.user.contact);
+      console.log(data.user.firstName);
+      localStorage.setItem('firstName', data.user.firstName);
+      console.log(data.user.lastName);
+      localStorage.setItem('lastName', data.user.lastName);
+      console.log(data.user.profile.bloodgroup);
+      localStorage.setItem('bloodgroup', data.user.profile.bloodgroup);
+      console.log(data.user.profile.points);
+      localStorage.setItem('points', data.user.profile.points);
+      console.log(data.accessToken);
+      localStorage.setItem('accessToken', data.accessToken);
+      console.log(data.user.profile.location);
+      localStorage.setItem('location', data.user.profile.location);
       console.log(data);
-      console.log(data);
+    },error =>{
+      alert("Unauthorized Access, try again.")
     });
   }
 
