ae9ca7eca45ad17eb6518aa14e815ea042076e76
ChamikaWickramaratne <chamika.h.wickramaratne@gmail.com>
2020-08-08 15:58:05 +0530

    register user

--- userinterface/ruhiru/src/app/register/register.component.ts
+++ userinterface/ruhiru/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,7 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  RegForm = new FormGroup({
+    fName: new FormControl(''),
+    lName: new FormControl(''),
+    email: new FormControl(''),
+    ContactNo: new FormControl(''),
+    Password1: new FormControl(''),
+    password2: new FormControl('')
+  });
+
+  onSubmit() {
+    var fname = this.RegForm.get("fName").value;
+    var lname = this.RegForm.get("lName").value;
+    var email = this.RegForm.get("email").value;
+    var password = this.RegForm.get("Password1").value;
+    var contact = this.RegForm.get("ContactNo").value;
+    let regData = {
+      "firstName":fname,
+      "lastName":lname,
+      "email":email,
+
+      "password":password,
+      "contact":contact
+    }
+    var url="https://d9edc04fdbc6.ngrok.io/api/v1/user/register"
+
+    this.http.post<any>(url,regData).subscribe(data =>{
+      console.log(data);
+    });
+  }
 
   ngOnInit(): void {
   }
