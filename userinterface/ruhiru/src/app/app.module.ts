ab3f56df7d5a877d7a5297811c1021e3d79c1bf4
nizzlo <nisalsamaranayake@gmail.com>
2020-08-08 12:10:01 +0530

    smaller changes

--- userinterface/ruhiru/src/app/app.module.ts
+++ userinterface/ruhiru/src/app/app.module.ts
@@ -4,16 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+//material styles
+import { MatSliderModule } from '@angular/material/slider';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    MatSliderModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
